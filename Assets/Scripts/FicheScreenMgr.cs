86529df9efb12037d165fbb56556abfc2334ee0d
JeanDev <hardyjea@gmail.com>
2020-07-25 00:36:49 +0200

    Fiche screen ok + timer ok

---
+++ Assets/Scripts/FicheScreenMgr.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FicheScreenMgr : MonoBehaviour
+{
+    public GameObject fiche;
+    public List<Sprite> ficheSpriteList = new List<Sprite>();
+
+    private int ficheIndex = 0;
+
+    void Start()
+    {
+        if (!fiche || ficheSpriteList.Count == 0) return;
+
+        fiche.GetComponent<Image>().sprite = ficheSpriteList[ficheIndex];
+    }
+
+    #region BUTTON FUNCTIONS
+    public void NextFiche()
+    {
+        ficheIndex++;
+
+        if(ficheIndex >= ficheSpriteList.Count)
+        {
+            ficheIndex = 0;
+        }
+
+        fiche.GetComponent<Image>().sprite = ficheSpriteList[ficheIndex]; 
+    }
+
+    public void PreviousFiche()
+    {
+        ficheIndex--;
+
+        if (ficheIndex < 0)
+        {
+            ficheIndex = ficheSpriteList.Count - 1;
+        }
+
+        fiche.GetComponent<Image>().sprite = ficheSpriteList[ficheIndex];
+    }
+    #endregion
+
+
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject ficheScreen;
+    public GameObject candidatsScreen;
+
+    public Text timerText;
+    public List<float> timerStartValues = new List<float>();
+    private float timerValue = 0;
+
+    private bool timerIsRunning = false;
+    private bool isStartValueUpdated = false;
+    private bool screenChanged = false;
+
+    private void Start()
+    {
+        timerIsRunning = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isStartValueUpdated)
+        {
+            if (ficheScreen.activeInHierarchy)
+            {
+                timerValue = timerStartValues[0];
+            }
+            else if (candidatsScreen.activeInHierarchy)
+            {
+                timerValue = timerStartValues[1];
+            }
+            else Debug.Log("Probleme avec le active in hierarchy");
+
+            isStartValueUpdated = true;
+            screenChanged = false;
+        }
+
+        if(timerIsRunning)
+        {
+            if (timerValue > 0)
+            {
+                timerValue -= Time.deltaTime;
+                DisplayTime(timerValue);
+            }
+            else
+            {
+                Debug.Log("Fin du timer !");
+
+                if (screenChanged) return;
+
+                if (ficheScreen.activeInHierarchy)
+                {
+                    ficheScreen.SetActive(false);
+                    candidatsScreen.SetActive(true);
+                    isStartValueUpdated = false;
+                    screenChanged = true;
+                }
+                else if (candidatsScreen.activeInHierarchy)
+                {
+                    candidatsScreen.SetActive(false);
+                    Debug.Log("Afficher score final");
+                    screenChanged = true;
+                }
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
