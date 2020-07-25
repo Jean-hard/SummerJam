f99b461bd4888dafe5c9ca864b0074bec855bcdf
JeanDev <hardyjea@gmail.com>
2020-07-25 22:34:25 +0200

    Add new metier in metier list

--- Assets/Scripts/Candidat.cs
+++ Assets/Scripts/Candidat.cs
@@ -8,7 +8,11 @@ public enum Metier
     Ingenieur,
     Stenographe,
     Historien,
-    GardeChausse
+    GardeChausse,
+    Batteur,
+    Gramatteur,
+    Planteur,
+    Animateur
 }
 
 [CreateAssetMenu(fileName = "Candidat", menuName = "Candidat")]
