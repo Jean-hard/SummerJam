1ea3fb24a5a10af08b9976c99536f223ada7dc71
JeanDev <hardyjea@gmail.com>
2020-07-25 10:54:16 +0200

    Created candidat screen visual

--- Assets/Scripts/CandidatScreenMgr.cs
+++ Assets/Scripts/CandidatScreenMgr.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Postuleur : MonoBehaviour
+public class CandidatScreenMgr : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/FicheScreenMgr.cs
+++ Assets/Scripts/FicheScreenMgr.cs
@@ -44,5 +44,4 @@ public class FicheScreenMgr : MonoBehaviour
     #endregion
 
 
-
 }
