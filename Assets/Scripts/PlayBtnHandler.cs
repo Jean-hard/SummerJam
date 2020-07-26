d7f4c4de0d0f2adfdc49fa67b9bee650ee7ab3e6
JeanDev <hardyjea@gmail.com>
2020-07-26 14:43:41 +0200

    Start screen done

---
+++ Assets/Scripts/PlayBtnHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PlayBtnHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Sprite onPlaySprite;
+    public Sprite outPlaySprite;
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        this.GetComponent<Image>().sprite = onPlaySprite;
+    }
+
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        this.GetComponent<Image>().sprite = outPlaySprite;
+    }
+}
